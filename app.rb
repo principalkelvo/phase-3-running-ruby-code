18fe90fd8db94ce7c644e459ec3e70a8db817eef
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-18 12:22:36 +0300

    introduction to ruby

---
+++ app.rb
@@ -0,0 +1,18 @@
+# puts "Hello world!"
+# app.rb
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+puts "Hello World!\n"
+puts "Hello world!"
+puts "Hello world!"
+puts [1, 2, 3]
+puts [1, 2, 3].inspect
+
+print "Pass this test, please."
+
+# This is the equivalent of calling: puts [1, 2, 3].inspect
+p [1, 2, 3]
+
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
